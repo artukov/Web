c484ba26f46cac30fe21dedb9999b26adb101893
Artukov <nikolartukov@gmail.com>
2020-07-28 13:32:12 +0200

    Login and Register forms

--- Frontend/web-app/src/views/LoginPage.vue
+++ Frontend/web-app/src/views/LoginPage.vue
@@ -1,5 +1,63 @@
 <template>
-  <div class="login">
-    <h1>Login page</h1>
+  <div class="position">
+    <b-form @submit.prevent="onSubmit()" method="post">
+      <b-form-group id="input-group-2" label="Username:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          type="text"
+          required
+          placeholder="Enter username"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-1" label="Password:" label-for="input-1">
+        <b-form-input
+          id="input-1"
+          v-model="form.password"
+          type="password"
+          required
+          placeholder="Enter password"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Log in</b-button>
+    </b-form>
+    <p></p>
+    <router-link to="/register">Dont have an account? Register now</router-link>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      form: {
+        password: "",
+        name: "",
+      },
+      info: null,
+    };
+  },
+  methods: {
+    onSubmit() {
+      var user = {
+        username: this.form.name,
+        password: this.form.password,
+      };
+      axios.post("http://localhost:8080/RACompany/rest/login", user);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.position {
+  width: 20%;
+  height: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 30px;
+}
+</style>
--- Frontend/web-app/src/views/RegisterPage.vue
+++ Frontend/web-app/src/views/RegisterPage.vue
@@ -1,5 +1,105 @@
 <template>
-  <div class="register">
-    <h1>Register page</h1>
+  <div class="position">
+    <b-form @submit.prevent="onSubmit" method="post">
+      <b-form-group id="input-group-2" label="Username:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.username"
+          required
+          placeholder="Enter username"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-1" label="Password:" label-for="input-1">
+        <b-form-input
+          id="input-1"
+          v-model="form.password"
+          type="password"
+          required
+          placeholder="Enter password"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Lastname:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.lastname"
+          required
+          placeholder="Enter lastname"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Gender:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="form.gender"
+          :options="genders"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Role:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="form.role"
+          :options="roles"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Register</b-button>
+    </b-form>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: "",
+        name: "",
+        lastname: "",
+        gender: "",
+        role: "",
+      },
+      genders: ["Male", "Female"],
+      roles: ["GUEST", "HOST"],
+    };
+  },
+  methods: {
+    onSubmit() {
+      var user = {
+        username: this.form.name,
+        password: this.form.password,
+        gender: this.form.gender,
+        userRole: this.form.role,
+        name: this.form.name,
+        lastname: this.form.lastname,
+      };
+      axios.post("http://localhost:8080/RACompany/rest/register", user);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.position {
+  width: 20%;
+  height: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 30px;
+}
+</style>
